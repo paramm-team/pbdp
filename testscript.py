150e7e181bdbab812da874eab2e6069a7c1e4c27
PipGrylls <pipgryllsastro@gmail.com>
2024-01-22 10:55:59 +0000

    linting

--- testscript.py
+++ testscript.py
@@ -4,11 +4,12 @@ from modules.states import add_state_label
 from modules.states import find_cc_and_cv
 my_instance = Parser()
 
-data = my_instance.data_importer(path_or_file='C:/Users/alex_/Downloads/Cell033_RPT_0p3C_0p3C_100_0_80cyc_10degC.csv', 
-                 state_option='', print_option='', save_option='', file_type='csv')
+data = my_instance.data_importer(
+    path_or_file='C:/Users/alex_/Downloads/Cell033_RPT_0p3C_0p3C_100_0_80cyc_10degC.csv',
+    state_option='', print_option='', save_option='', file_type='csv')
 
 list_df = segment_data(data, ["dischg"])
-#print(list_df[0]["Current [A]"], list_df[1]["Voltage Full [V]"], list_df[2])
+# print(list_df[0]["Current [A]"], list_df[1]["Voltage Full [V]"], list_df[2])
 print(list_df)
 print(len(list_df))
 
@@ -20,4 +21,3 @@ print(len(list_df))
 
 # def test_add_negative_numbers():
 #     assert -2 + -3 == -5
- 
