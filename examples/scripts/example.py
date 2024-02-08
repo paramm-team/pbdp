c34a5663b68d5f5788daac90477e4b0215fd4361
PipGrylls <pipgryllsastro@gmail.com>
2024-02-08 11:19:06 +0000

    removing examples from nox

--- examples/scripts/example.py
+++ examples/scripts/example.py
@@ -2,12 +2,14 @@ from pathlib import Path
 import pbdp
 from pbdp import segment
 
-data_file = Path(pbdp.__path__[0], "input", "data", "Digatron.csv")
 
-parser = pbdp.Parser()
-data = parser.data_importer(
-    path_or_file=data_file,
-    file_type='csv'
-)
-print(segment.segment_data(data=data, requests=["cccv, rest"]))
-#print(data["Step Number"])
+def test_simple():
+    data_file = Path(pbdp.__path__[0], "input", "data", "Digatron.csv")
+
+    parser = pbdp.Parser()
+    data = parser.data_importer(
+        path_or_file=data_file,
+        file_type='csv'
+    )
+    print(segment.segment_data(data=data, requests=["cccv, rest"]))
+    #print(data["Step Number"])
