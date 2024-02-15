55c4061c2fabce6c2a35e85040a8f1493be9c1ef
Alexandru Pascu <alexandru.pascu99@gmail.com>
2024-02-15 14:15:57 +0000

    Cycler Detection & Absolute Time
    Adding the functionality to detect the cycler based on the keywords found and the option for the user to tell it or the line from where data starts and metadata finishes.
    
    Implemented Absolute time detection taking it from the available recorded testing data or the metadata of the experiment/file.
    
    Adjusted the logger for the new changes

--- pbdp/pbdp_parser.py
+++ pbdp/pbdp_parser.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime, timedelta
 
 import chardet
 import openpyxl
@@ -114,6 +115,7 @@ class Parser:
                     "Potential (V)",
                     "Ewe-Ece/V",
                     "Ecell/V",
+                    " Ecell/V"
                     "Voltage (V)",
                     "Voltage (v)",
                     " Voltage (V)",
@@ -200,11 +202,11 @@ class Parser:
         if path_or_file.is_file():
             # If the input is a file, check if it's not empty
             if path_or_file.stat().st_size > 1:
+                self.logger.debug("File found")
                 return [path_or_file]
             else:
                 self.logger.warning(f"Empty file, {path_or_file} ")
                 raise ValueError(f"Empty file: {path_or_file}")
-            self.logger.debug("File found")
         elif path_or_file.is_dir():
             # If the input is a directory, scan for non-empty files
             files = [
@@ -257,7 +259,7 @@ class Parser:
         self.logger.debug(f"Excel file, {file_path}, converted to CSV, {csv_file_path}")
         return csv_file_path
 
-    def find_words(self, file_path: Path) -> tuple:
+    def find_words(self, file_path: Path, cycler) -> tuple:
         """
         Searches the file contents for specific keywords related to different types of equipment.
         The search is performed iteratively for each equipment type defined in self.cycler_keywords.
@@ -286,19 +288,53 @@ class Parser:
                 self.logger.info(f"Encoding detected: {encoding}")
                 contents = contents.decode(encoding)
                 self.logger.info("File content read")
-        # Iterate over each equipment type in the cycler_keywords dictionary
-        for equipment_type, keywords in self.cycler_keywords.items():
+
+        if not cycler:
+            # Iterate over each equipment type in the cycler_keywords dictionary
+            for equipment_type, keywords in self.cycler_keywords.items():
+                patterns = []
+                for phrase in keywords:
+                    # Split the phrase into words and escape each word
+                    escaped_words = [re.escape(word) for word in phrase.split()]
+                    # Join the words with '\s*' to allow for flexible whitespace
+                    pattern = r'\s*'.join(escaped_words)
+                    patterns.append(pattern)
+
+                # Compile a regex pattern for the current set of keywords/phrases
+                full_pattern = re.compile("|".join(patterns))
+
+                # Search for the keywords in the file contents
+                match = full_pattern.search(contents)
+                if match:
+                    with open(file_path, "rb") as f:
+                        # If a match is found, set the file pointer to that location and return the position and the equipment type
+                        f.seek(match.start())
+                        self.logger.info(f"Keywords found in file, {file_path}, at position\
+                                 {match.start()}, pointer set")
+                        return (f.tell(), encoding, equipment_type)
+        # If the line number is given for the header of the line set the file pointer to that location
+        elif cycler.isnumeric():
+            cycler = int(cycler)
+            with open(file_path, "rb") as f:
+                current_line = 1
+                while current_line < cycler:
+                    line = f.readline()
+                    if line == b'':
+                        # Reached EOF before the desired line number
+                        return -1
+                    current_line += 1
+                # The file pointer is now at the beginning of the desired line
+                return (f.tell(), encoding, 'cannot determine')
+        elif cycler in self.cycler_keywords.keys():
             patterns = []
-            for phrase in keywords:
+            for phrase in self.cycler_keywords[cycler]:
                 # Split the phrase into words and escape each word
                 escaped_words = [re.escape(word) for word in phrase.split()]
                 # Join the words with '\s*' to allow for flexible whitespace
                 pattern = r'\s*'.join(escaped_words)
                 patterns.append(pattern)
-        
             # Compile a regex pattern for the current set of keywords/phrases
             full_pattern = re.compile("|".join(patterns))
-    
             # Search for the keywords in the file contents
             match = full_pattern.search(contents)
             if match:
@@ -307,13 +343,12 @@ class Parser:
                     f.seek(match.start())
                     self.logger.info(f"Keywords found in file, {file_path}, at position\
                              {match.start()}, pointer set")
-                    return (f.tell(), encoding, equipment_type)
-            else: 
-                 # If no match is found, raise an exception
-                self.logger.warning(f"No keywords found in file, {file_path}")
-                raise ValueError(f"No keywords found in file: {file_path}")
+                    return (f.tell(), encoding, cycler)
+        else:
+            print("This cycler is not yet supported")
+                
         # If no match is found, raise an exception
-        raise ValueError(f"No keywords found in file: {file_path}")
+        self.logger.warning(f"No keywords found in file, {file_path}")
 
     def split_file(self, pointer: int, file_path: Path, save_option: str) -> tuple:
         """
@@ -431,7 +466,6 @@ class Parser:
         elif file_ext == ".txt":
             df = pd.read_csv(temp_file, sep="\t", encoding=encoding)
         elif file_ext == ".mpt":
-            # df = pd.read_table(temp_file, encoding=encoding)
             df = pd.read_csv(temp_file, sep="\t", encoding=encoding)
         elif file_ext == ".DTA":
             df = pd.read_table(temp_file, sep="\t", encoding=encoding)
@@ -493,20 +527,35 @@ class Parser:
             pd.DataFrame: A new DataFrame with column headers converted
                             according to standard_headers.
         """
+        def normalize_header(header):
+            """Normalize a header to a common format."""
+            # Lowercase and remove special characters
+            header = header.lower()
+            header = re.sub(r'\s+|\[|\]|/|,', '', header)
+            return header
 
         # Get the original column names
         original_headers = data.columns
         # Create a dictionary to store the new header names
         new_headers = {}
-
+    
+        #normalize headers in the document
         for col in original_headers:
-            for header, value in self.standard_headers.items():
-                # Check if the original header matches the mapping
-                if col in value:
-                    new_headers[col] = header
-                    break
-        self.logger.info("Headers converted to standard")
+            normalized_col = normalize_header(col)
+            matched = False
 
+            for header, values in self.standard_headers.items():
+                normalized_values = [normalize_header(value) for value in values]
+                # Check if the original header matches the mapping
+                if normalized_col in normalized_values:
+                    new_headers[col] = header
+                    matched = True
+                    break
+
+            if not matched:
+                self.logger.info(f"No standard header found for: {col}")
+
+        self.logger.info("Headers converted to standard")
         # Rename columns using the new mapping
         data.rename(columns=new_headers, inplace=True)
 
@@ -516,6 +565,7 @@ class Parser:
                 data[col] = pd.to_numeric(data[col], errors="coerce")
 
         self.logger.info("Columns converted to numeric")
+
         return data
 
     def remove_unwanted(self, data: pd.DataFrame) -> pd.DataFrame:
@@ -526,8 +576,7 @@ class Parser:
             df (pd.DataFrame): The DataFrame containing data to be processed.
 
         Returns:
-            pd.DataFrame: A new DataFrame with unwanted rows and columns
-                            removed.
+            pd.DataFrame: A new DataFrame with unwanted rows and columns removed.
         """
 
         self.logger.info("Removing unwanted rows and columns")
@@ -550,7 +599,110 @@ class Parser:
         self.logger.info("Unwanted rows and columns removed")
         return data
     
-    def sanity_check(self, data: pd.DataFrame) -> str:
+    def find_start_time(self, metadata: str) -> str:
+        """
+        Searches the provided metadata for a start time of an experiment using various patterns.
+
+        Args:
+            metadata (str): A string containing the experiment's metadata, which may include
+                            the start time or date in various formats.
+
+        Returns:
+            str or None: The extracted start time as a string if a match is found; otherwise, None.
+        """
+        # List of regular expression patterns for matching start time/date in metadata.
+        # Each pattern accounts for a specific format that the start time/date might be recorded in.
+        patterns = [
+            r"Start Time,?(\d+/\d+/\d+ \d+:\d+:\d+ [APM]+)",  # Matches Start Time
+            r"Test Date:?,?\"?(\d+/\d+/\d+ \d+:\d+:\d+)",  # Matches Test Date
+            r"Start date:, (\d+ \w+ \d+)",  # Matches Start date
+            r"Start time:, (\d+:\d+:\d+ \+\d+:\d+)",  # Matches Start time with timezone offset
+            r"Technique started on : (\d+/\d+/\d+ \d+:\d+:\d+)",  # Matches Technique started on
+            r"DATE\tLABEL\t(\d+/\d+/\d+)",  # Matches DATE with tab-separated LABEL
+            r"TIME\tLABEL\t(\d+:\d+:\d+)",  # Matches TIME with tab-separated LABEL
+            r"Date of Test:?,?\"?(\d+ \w+ \d+, \d+:\d+:\d+ [APM]+)",  # Matches Date of Test
+            r"Started,(\d+/\d+/\d+ \d+:\d+)",  # Matches Started
+            r"Modify on : (\d+/\d+/\d+ \d+:\d+)",  # Matches Modify on
+            r"Acquisition started on : (\d+/\d+/\d+ \d+:\d+:\d+)",  # Matches Acquisition started on
+            r"Date,(\d+-\d+-\d+ \d+:\d+:\d+),",  # Matches Date in ISO format
+            r"Time,(\d+:\d+:\d+),"  # Matches Time
+        ]
+
+        # Iterate over each pattern, searching for a match in the metadata.
+        for pattern in patterns:
+            match = re.search(pattern, metadata, re.IGNORECASE)
+            if match:
+                # If a match is found, return the matched group, which is the date/time.
+                self.logger.info("Date and time found in metadata.")
+                return match.group(1)
+
+        # If no matches are found, return None.
+            self.logger.warning("Date and time not found in metadata.")
+        return None
+    
+    def absolute_time(self, metadata:bytes, data: pd.DataFrame, encoding: str) -> pd.DataFrame:
+        """
+        Adds an 'Absolute Time [s]' column to the provided DataFrame based on the experiment's
+        start time found in metadata. The method attempts to identify the start time from various
+        potential formats in the metadata. If successful, it calculates absolute time for each
+        row in the data DataFrame and adds this as a new column.
+
+        Args:
+            metadata (bytes): Metadata containing the experiment's start time.
+            data (pd.DataFrame): DataFrame containing the experiment data, including a "Time [s]" column
+                                 which is cumulative time since the start of the experiment.
+            encoding (str): Encoding of the metadata if provided as bytes. Default is 'utf-8'.
+
+        Returns:
+            pd.DataFrame: The input DataFrame with an added 'Absolute Time [s]' column.
+
+        Raises:
+            ValueError: If the start time format is not recognized or cannot be found in metadata.
+        """
+
+        # Check if 'Absolute Time [s]' is already present in the DataFrame.
+        if "Absolute Time [s]" in data.columns:
+            return data
+
+        # Decode metadata from bytes to string if necessary.
+        if isinstance(metadata, bytes):
+            metadata = metadata.decode(encoding)
+
+        # Attempt to find start time from metadata.
+        start_time_str = self.find_start_time(metadata)
+
+        # Additional logic for combining separate date and time entries in metadata.
+        # This part is crucial for cases where the date and time are provided separately and need to be concatenated.
+        combined_patterns = [
+            (r"Start date:, (\d+ \w+ \d+)", r"Start time:, (\d+:\d+:\d+ \+\d+:\d+)"),
+            (r"DATE\tLABEL\t(\d+/\d+/\d+)", r"TIME\tLABEL\t(\d+:\d+:\d+)"),
+            (r"Date,(\d+-\d+-\d+ \d+:\d+:\d+,)", r"Time,(\d+:\d+:\d+,)")
+        ]
+        for date_pat, time_pat in combined_patterns:
+            if re.search(date_pat, metadata) and re.search(time_pat, metadata):
+                date_str = self.find_start_time(date_pat + metadata.split(date_pat)[-1])
+                time_str = self.find_start_time(time_pat + metadata.split(time_pat)[-1])
+                if date_str and time_str:
+                    start_time_str += " " + time_str  # Combine date and time.
+
+        # Convert start time string to datetime object using various potential formats.
+        for fmt in ("%d/%m/%Y %H:%M:%S", "%d %B %Y %H:%M:%S", "%Y-%m-%d %H:%M:%S", "%d/%m/%Y %H:%M", "%d %B %Y, %I:%M:%S %p"):
+            try:
+                start_time = datetime.strptime(start_time_str, fmt)
+                break
+            except ValueError:
+                continue
+        else:
+            self.logger.warning("Start time format not recognized or start time not found in metadata.")
+            raise ValueError("Start time format not recognized or start time not found in metadata.")
+
+        # Calculate and add 'Absolute Time [s]' to the DataFrame.
+        data["Absolute Time [s]"] = data["Time [s]"].cumsum().apply(lambda x: start_time + timedelta(seconds=x))
+        self.logger.info("Absolute time added to the dataframe")
+
+        return data
+    
+    def sanity_check(self, data: pd.DataFrame) -> int:
         """
         Performs a sanity check on the provided DataFrame to determine if it is a
         normal experiment or a frequency experiment and ensures the presence of necessary data.
@@ -563,13 +715,14 @@ class Parser:
             data (pd.DataFrame): The DataFrame to be checked. It should contain data from experiments.
 
         Returns:
-            str: A confirmation message indicating that the sanity check has passed.
+            int: A confirmation 0 integer saying the sanity check has passed.
 
         Raises:
             ValueError: If any required columns are missing based on the determined experiment type.
+                        or if the three electrode data doesn't make sense working - counter = voltage
         """
         check_normal = ["Current [A]", "Time [s]"]
-        check_either_normal = [("Voltage [V]", "Working electrode potential [V]")]
+        check_either_normal = [("Voltage [V]", "Working electrode potential [V]", "Counter electrode potential [V]")]
         check_freq = ["freq [Hz]", "ReZ [Ohm]", "ImZ [Ohm]"]
 
         # Determine if the data is likely a frequency experiment
@@ -581,23 +734,37 @@ class Parser:
             # Check for missing columns in frequency experiments
             missing_freq = [col for col in check_freq if col not in data.columns]
             if missing_freq:
+                self.logger.warning(f"Frequency data is missing {missing_cols} for experiments")
                 raise ValueError(f"Your frequency data is missing the following columns: {', '.join(missing_freq)}")
         else:
             # Check for missing columns in normal experiments
             missing_normal = [col for col in check_normal if col not in data.columns]
             missing_either = all(col not in data.columns for col_pair in check_either_normal for col in col_pair)
 
+            # Raise error noting the missing columns from the data
             if missing_normal or missing_either:
                 missing_cols = ', '.join(missing_normal)
                 if missing_either:
                     missing_cols += '; At least one of each pair is required: ' + ', '.join([' or '.join(pair) for pair in check_either_normal])
+                self.logger.warning(f"Data is missing {missing_cols} for experiments")
                 raise ValueError(f"Your data is missing the following columns for normal experiments: {missing_cols}")
-
-        return "Sanity check passed!"
+        
+        # Check that the thre electrode voltage data makes sense
+        three_el = ["Working electrode potential [V]", "Counter electrode potential [V]", "Voltage [V]"]
+        if all(elem in data.columns for elem in three_el):
+            check = (data["Working electrode potential [V]"].abs() - 
+             data["Counter electrode potential [V]"].abs()).abs()
+            if not (check - data["Voltage [V]"]).abs().lt(0.02).all():
+                self.logger.warning("Voltage data in three electrode experiment is corrupted")
+                raise ValueError("Your voltage data is corrupted")
+            
+        self.logger.info("Sanity check passed")
+        return 0
 
     def data_importer(
         self,
         path_or_file: Path,
+        cycler: str = "",
         file_type: str = "csv",
         save_option: str = "save",
         state_option: str = "",
@@ -622,7 +789,7 @@ class Parser:
         for file in self.look_for_files(path_or_file):
             # Process each file
             self.logger.info(f"Processing file, {file}")
-            pointer, encoding, equipment_type = self.find_words(file)
+            pointer, encoding, equipment_type = self.find_words(file, cycler)
             self.logger.info(f"Pointer and encoding found for file, {file}")
             metadata, data = self.split_file(pointer, file, save_option)
             self.logger.info(f"File, {file}, split into metadata and data")
@@ -631,6 +798,7 @@ class Parser:
             data = self.change_headers(data)
             data = self.remove_unwanted(data)
             try:
+                data = self.absolute_time(metadata, data, encoding)
                 self.sanity_check(data)
             except Exception as e:
                 self.logger.warn(f"An error occurred: {e} when processing {file}")
